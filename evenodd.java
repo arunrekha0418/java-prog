3e7e1ce670e22eab2b973d192a6cc0b7f7561f71
arunrekha0418 <42768709+arunrekha0418@users.noreply.github.com>
2018-09-25 09:18:52 +0530

    Create evenodd.java

---
+++ evenodd.java
@@ -0,0 +1,21 @@
+/*Even or Odd*/
+
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int num;
+		Scanner sacn=new Scanner(System.in);
+		num=sacn.nextInt();
+		{
+			if (num % 2==0)
+			System.out.println("even");
+			else
+			System.out.println("odd");
+		}
+		
+	}
+}
